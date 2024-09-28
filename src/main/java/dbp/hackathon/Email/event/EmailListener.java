da37d48f768de2aa12000f0f7c6d03d54231d8cf
fdavilaventuro <fabio.davila@utec.edu.pe>
2024-09-28 16:15:32 -0500

    added email

--- src/main/java/dbp/hackathon/Email/event/EmailListener.java
+++ src/main/java/dbp/hackathon/Email/event/EmailListener.java
@@ -1,4 +1,18 @@
 package dbp.hackathon.Email.event;
 
+import dbp.hackathon.Email.domain.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Component
 public class EmailListener {
+    @Autowired
+    private EmailService emailService;
+
+    @EventListener
+    @Async
+    public void handleEmail(EmailEvent event) {
+    }
 }
