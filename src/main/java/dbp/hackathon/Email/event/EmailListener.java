e11b3d8668d19abd305bd0431f495e0b90c512cc
MikaelQuintoR <MIKAEL.QUINTO@UTEC.EDU.PE>
2024-09-28 16:15:11 -0500

    Email Creation

--- src/main/java/dbp/hackathon/Email/event/EmailListener.java
+++ src/main/java/dbp/hackathon/Email/event/EmailListener.java
@@ -1,5 +1,18 @@
 package dbp.hackathon.Email.event;
 
-public class EmailListener {
 
+import dbp.hackathon.Email.domain.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EventListener {
+    @Autowired
+    private EmailService emailService;
+
+    @org.springframework.context.event.EventListener
+    @Async
+    public void handleHelloEmailEvent(EmailEvent event){
+        emailService.sendMesage(event.getEmail(),event.getSubject(),event.getContent());
+    }
 }
